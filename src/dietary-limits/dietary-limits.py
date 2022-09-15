dc25a8d6d8dd2d22dea36f902939aefb3d71e8f6
pritchardn <21726929@student.uwa.edu.au>
2022-09-15 17:21:35 +0800

    Adds starting of nutrition levels implementation

--- src/dietary-limits/dietary-limits.py
+++ src/dietary-limits/dietary-limits.py
@@ -4,6 +4,7 @@ Contains classes returning dietary limits based on nhmrc guidelines
 @author Nicholas Pritchard nicholas.pritchard@uwa.edu.au
 """
 import enum
+from pydantic import BaseModel
 
 
 class Sex(enum):
@@ -11,13 +12,67 @@ class Sex(enum):
     Female = 1
 
 
-class Pregnant(enum):
-    NA = 0
-    Trimester1 = 1
-    Trimester2 = 2
-    Trimester3 = 3
-    Lactating = 4
+class Unit(enum):
+    ug = 6
+    mg = 3
+    g = 1
 
 
-def nutrition_limits(age: int, sex: Sex, height: int, weight: float, activity_level: float, pregnancy_state: Pregnant):
-    return {}
+class Nutrient(BaseModel):
+    rdi: float
+    ul: float = None
+    unit: str
+
+
+class Human(BaseModel):
+    age: int
+    sex: Sex
+    height: int
+    weight: float
+    activity: float
+
+
+class NutritionLevels(BaseModel):
+    energy: Nutrient(unit="Kj")
+    protein: Nutrient(unit="g/kg")
+    fibre: Nutrient(unit="g")
+    water: Nutrient(unit="mL")
+    # Carbs can be taken as the rest
+    # Fats
+    alinoleic: Nutrient(unit="g")
+    linoleic: Nutrient(unit="g")
+    n3fat: Nutrient(unit="mg")
+    # Vitamins
+    vitamin_a: Nutrient(unit="ug")
+    thiamin: Nutrient(unit="mg")
+    riboflavin: Nutrient(unit="mg")
+    niacin: Nutrient(unit="mg")
+    vitamin_b6: Nutrient(unit="mg")
+    vitamin_b12: Nutrient(unit="ug")
+    folate: Nutrient(unit="ug")
+    pantothenic_acid: Nutrient(unit="mg")
+    biotin: Nutrient(unit="ug")
+    choline: Nutrient(unit="mg")
+    vitamin_c: Nutrient(unit="mg")
+    vitamin_d: Nutrient(unit="ug")
+    vitamin_e: Nutrient(unit="mg")
+    vitamin_k: Nutrient(unit="ug")
+    # Minerals
+    calcium: Nutrient(unit="mg")
+    chromium: Nutrient(unit="ug")
+    copper: Nutrient(unit="mg")
+    fluoride: Nutrient(unit="mg")
+    iodine: Nutrient(unit="ug")
+    iron: Nutrient(unit="mg")
+    magnesium: Nutrient(unit="mg")
+    manganese: Nutrient(unit="mg")
+    molybdenum: Nutrient(unit="ug")
+    phosphorus: Nutrient(unit="mg")
+    potassium: Nutrient(unit="mg")
+    selenium: Nutrient(unit="ug")
+    sodium: Nutrient(unit="mg")
+    zinc: Nutrient(unit="mg")
+
+
+def nutrition_limits(human_model: Human) -> NutritionLevels:
+    return NutritionLevels()

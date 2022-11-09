8cf13f3bc4f44c4926ce6c8348592fd8e704a006
pritchardn <21726929@student.uwa.edu.au>
2022-11-09 16:47:57 +0800

    Adds all diet modes to plots.

--- src/plotting/plot.py
+++ src/plotting/plot.py
@@ -10,12 +10,12 @@ from matplotlib.colors import rgb2hex
 import matplotlib.colors as colors
 from parse import *
 
-from src.dietary_limits.dietary_limits import Sex
+from src.dietary_limits.dietary_limits import Sex, Restriction
 
 
-def plot(sex: Sex, bmis, ages, activities, costs, whole_min, whole_max):
+def plot(sex: Sex, bmis, ages, activities, costs, whole_min, whole_max, restriction):
     fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={"projection": "3d"})
-    fig.suptitle(f"{sex.name.title()} diet costs")
+    fig.suptitle(f"{sex.name.title()} diet costs - {restriction}")
     cmap = cm.plasma
     norm = colors.Normalize(vmin=whole_min, vmax=whole_max)
     m = cm.ScalarMappable(norm=norm, cmap=cmap)
@@ -73,28 +73,33 @@ def get_diet_cost(filename: str) -> float:
 def process_files(filenames: list, sex:Sex) -> tuple[list, list, list, list]:
     bmis, ages, activities, costs = [], [], [], []
     for filename in filenames:
+        cost = get_diet_cost(filename)
+        if cost == 0.0:
+            continue
         bmi, age, activity = filename_to_params(filename, sex)
         bmis.append(bmi)
         ages.append(age)
         activities.append(activity)
-        costs.append(get_diet_cost(filename))
+        costs.append(cost)
     return bmis, ages, activities, costs
 
 
 def main():
-    data_path = "../../results"
-    male_files = glob.glob(f"{data_path}{os.sep}Male-*.out")
-    female_files = glob.glob(f"{data_path}{os.sep}Female-*.out")
-    print(len(male_files))
-    print(len(female_files))
-    bmis_m, ages_m, activities_m, costs_m = process_files(male_files, Sex.Male)
-    bmis_f, ages_f, activities_f, costs_f = process_files(female_files, Sex.Female)
-    whole_min = min([min(costs_m), min(costs_f)])
-    whole_max = max([max(costs_m), max(costs_f)])
+    for restriction in Restriction:
+        restriction = Restriction(restriction)
+        data_path = f"../../results/{restriction.name}"
+        male_files = glob.glob(f"{data_path}{os.sep}Male-*.out")
+        female_files = glob.glob(f"{data_path}{os.sep}Female-*.out")
+        print(len(male_files))
+        print(len(female_files))
+        bmis_m, ages_m, activities_m, costs_m = process_files(male_files, Sex.Male)
+        bmis_f, ages_f, activities_f, costs_f = process_files(female_files, Sex.Female)
+        whole_min = min([min(costs_m), min(costs_f)])
+        whole_max = max([max(costs_m), max(costs_f)])
 
-    print("Processed files")
-    plot(Sex.Male, bmis_m, ages_m, activities_m, costs_m, whole_min, whole_max)
-    plot(Sex.Female, bmis_f, ages_f, activities_f, costs_f, whole_min, whole_max)
+        print("Processed files")
+        plot(Sex.Male, bmis_m, ages_m, activities_m, costs_m, whole_min, whole_max, restriction.name)
+        plot(Sex.Female, bmis_f, ages_f, activities_f, costs_f, whole_min, whole_max, restriction.name)
 
 
 if __name__ == "__main__":
